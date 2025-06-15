3d2c78752ded5fd9e2458795a223c02f72ace96f
pe3iva <dawgsofast@protonmail.com>
2025-06-15 22:29:56 +0200

    initial commit

---
+++ src/goTo.cpp
@@ -0,0 +1,20 @@
+#include "goTo.h"
+#include <iostream>
+
+int goTo(int destination) {
+
+	if (destination == 1) {
+		std::cout << "1";
+	}
+	else if (destination == 2) {
+		std::cout << "2";
+	}
+	else if (destination == 3) {
+		std::cout << "3";
+	}
+	else {
+		std::cout << "4";
+	}
+
+	return destination;
+}
---
+++ src/goTo.h
@@ -0,0 +1,6 @@
+#ifndef GO_TO_H
+#define GO_TO_H
+
+int goTo(int destination);
+
+#endif
