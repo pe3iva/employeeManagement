48072aaf70b929732b0e8c8d85b462bfa88f371a
pe3iva <dawgsofast@protonmail.com>
2025-06-16 09:28:13 +0200

    add back function

--- addEmpInfo.cpp
+++ addEmpInfo.cpp
@@ -3,16 +3,26 @@
 
 void addEmployeeInfo() {
 
-	std::cout << "Employee name: ";
-	std::string employeeName{};
-	std::cin >> employeeName;
+	std::string goBack{};
 
-	std::cout << "Employee age: ";
-	int employeeAge{};
-	std::cin >> employeeAge;
+	do {
+		std::cout << "Employee name: ";
+		std::string employeeName{};
+		std::cin >> employeeName;
 
-	std::cout << "Employee role: ";
-	std::string employeeRole{};
-	std::cin >> employeeRole;
+		std::cout << "Employee age: ";
+		int employeeAge{};
+		std::cin >> employeeAge;
 
+		std::cout << "Employee role: ";
+		std::string employeeRole{};
+		std::cin >> employeeRole;
+
+		std::cout << "\n\nCreated new employee information card.\n\n\n";
+
+		std::cout << "You can exit by typing back, type anything else to continue: ";
+		std::cin >> goBack;
+
+	} while (goBack != "back");
+	
 }
